d20ef23ac5cab687ce2c2276dc3d14c6491dd484
R.komatsu <majaj62525@icloud.com>
2017-07-11 14:53:23 +0900

    update to file

--- KeyInput/KeyBoard.cpp
+++ KeyInput/KeyBoard.cpp
@@ -15,31 +15,41 @@ namespace User
 	{
 		if (!Layer::init()) { return false; }
 
+		//画像を用意
 		auto s = Sprite::create("HelloWorld.png");
 		s->setPosition(Director::getInstance()->getVisibleSize() / 2);
 		this->addChild(s);
 		this->setSprite(s);
 
+		//キーボード用のイベントリスナーを作成
 		auto listener = EventListenerKeyboard::create();
-		listener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* keyEvent) {
-		
+
+		//キーボードを押したときの処理( 離した瞬間の時はlistener->onKeyReleased )
+		listener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* keyEvent) {	
 			switch (keyCode)
 			{
+				//左キー
 			case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
 
 				sprite->setPositionX(sprite->getPositionX() - 1);
 
 				break;
+				
+				//右キー
 			case EventKeyboard::KeyCode::KEY_RIGHT_ARROW: 
 
 				sprite->setPositionX(sprite->getPositionX() + 1);
 
 				break;
+			
+				//上キー
 			case EventKeyboard::KeyCode::KEY_UP_ARROW:
 
 				sprite->setPositionY(sprite->getPositionY() + 1);
 
 				break;
+
+				//下キー
 			case EventKeyboard::KeyCode::KEY_DOWN_ARROW:
 
 				sprite->setPositionY(sprite->getPositionY() - 1);
@@ -49,6 +59,7 @@ namespace User
 				break;
 			}
 		};
+		//キーボードのイベントリスナーの登録
 		this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
 
 
@@ -58,6 +69,7 @@ namespace User
 
 	void KeyBoardInput::update(float delta)
 	{
+		//ログ表示
 		log("SpritePosition  x=%f y=%f", sprite->getPositionX(), sprite->getPositionY() );
 	}
 
